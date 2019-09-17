850eb4464d4075b677dc47a11aac77702cd80fd7
Peter Bangdiwala <peter.bangdiwala@abarcahealth.com>
2019-09-17 00:53:39 -0400

    writting basic skeleton

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## This function will create an object with a special matrix with the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    message("functions created. returning list")
+    list(set = set, 
+         get = get, 
+         setsolve = setsolve, 
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve will control the cache if the solve() has already been called for x
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if (!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)  # the actual place to solve
+    x$setsolve(m)
+    m
 }
